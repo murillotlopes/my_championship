492eab55b6c243ae2703d2d73b8669de35002fc8
murillotlopes <murillo.tlopes@hotmail.com>
2024-12-17 14:28:47 -0300

    fix(semi-final-usecase): semi-final result included

--- src/core/championship/model/semi-final.output.ts
+++ src/core/championship/model/semi-final.output.ts
@@ -2,6 +2,7 @@ import { BracketModel } from '../../bracket/model/bracket.model';
 
 export interface SemiFinalOutput {
 
+  semi_final: BracketModel[]
   final: BracketModel
   third_place_playoff: BracketModel
 
