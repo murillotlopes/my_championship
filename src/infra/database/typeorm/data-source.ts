d6239996d8b2724ff3058552083bc7be50d0bac3
murillotlopes <murillo.tlopes@hotmail.com>
2024-12-17 16:50:13 -0300

    test(test-config): setting adjustments

--- src/infra/database/typeorm/data-source.ts
+++ src/infra/database/typeorm/data-source.ts
@@ -1,12 +1,16 @@
 import path from 'node:path';
 import { DataSource } from 'typeorm';
 
+const schema = process.env.NODE_ENV === 'test' ? 'test' : 'public'
+const synchronize = process.env.NODE_ENV == 'test'
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  url: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-  synchronize: false,
-  entities: [path.join(__dirname, "/entities/**/*.{ts,js}")],
-  migrations: [path.join(__dirname, "/migrations/**/*.{ts,js}")],
-})
+export const AppDataSource =
+  new DataSource({
+    schema,
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    synchronize: synchronize,
+    entities: [path.join(__dirname, "/entities/**/*.{ts,js}")],
+    migrations: [path.join(__dirname, "/migrations/**/*.{ts,js}")],
+  })
