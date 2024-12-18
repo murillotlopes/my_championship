95aa8940ccd348faa785620eb2e41e958b0c6fdb
murillotlopes <murillo.tlopes@hotmail.com>
2024-12-18 10:11:47 -0300

    fix(draw-matches): corrected object formation

--- src/core/bracket/model/draw-matches.output.ts
+++ src/core/bracket/model/draw-matches.output.ts
@@ -1,5 +1,5 @@
 import { BracketModel } from './bracket.model';
 
-export interface DrawMatchesOutput extends Omit<BracketModel, 'id' | 'created_at' | 'updated_at' | 'championship' | 'realized'> {
+export interface DrawMatchesOutput extends Omit<BracketModel, 'id' | 'created_at' | 'updated_at' | 'championship' | 'realized' | 'team_a_points' | 'team_b_points'> {
 
 }
